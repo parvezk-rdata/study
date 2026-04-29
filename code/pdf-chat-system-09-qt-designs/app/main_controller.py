3796a395c2a34246cc03cbcee7a85c4de84ca0e8
parvezk <parvezk>
2026-04-29 23:39:26 +0530

    new chat pdf

--- code/pdf-chat-system-09-qt-designs/app/main_controller.py
+++ code/pdf-chat-system-09-qt-designs/app/main_controller.py
@@ -15,34 +15,23 @@ from app.models.state.app_state import AppState
 class MainController:
 
     def __init__(self, window: QMainWindow):
-
-        # --- Build UI ---
-        self._ui: UIBundle = UIComposer().build(window)
-
-        # --- Build Services ---
-        self._svc: ServiceBundle = ServiceComposer().build()
-
-        # --- Initialize state ---
-        self._state = AppState()
-
-        # --- Bind signals to handlers ---
-        self._bind_signals()
-
-    # -------------------------------------------------------------------------
-    # Signal Binding
-    # -------------------------------------------------------------------------
+        self._ui: UIBundle = UIComposer().build(window)         # Build UI 
+        self._svc: ServiceBundle = ServiceComposer().build()    # Build Services 
+        self._state = AppState()                                # Initialize state 
+        self._bind_signals()                                    # Bind signals to handlers 
 
     @property
     def ui(self) -> UIBundle:
         return self._ui
 
+    # Bind all signals(events to controller methods)
     def _bind_signals(self):
         self._ui.toolbar.bind_upload_requested( self._on_upload_clicked )
         self._ui.toolbar.bind_clear_clicked( self._on_clear_clicked )
-        # theme_changed
-        # self._ui.toolbar.bind_theme_changed( )
         self._ui.status_bar.bind_dismissed( self._on_status_bar_dismissed )
         self._ui.input_bar.bind_send_clicked( self._on_send_clicked )
+        # theme_changed
+        # self._ui.toolbar.bind_theme_changed( )
 
     # -------------------------------------------------------------------------
     # Event Handlers
